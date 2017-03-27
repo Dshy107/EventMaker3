03bbd54982a5f10b57b1b3684dd46e5baf20e0ce
Dshy <KortSkg@KORTSKG-PC>
2017-03-27 10:18:58 +0200

    Har tilføjet en azure database med elementer i

---
+++ Eventmakerinsert.sql
@@ -0,0 +1,12 @@
+﻿--CREATE TABLE Begivenhed(
+-- 	Event_Id  int  NOT NULL PRIMARY KEY,
+-- 	Name  	VARCHAR(30) 	NOT NULL,
+-- 	Description   VARCHAR(255)  NOT NULL,
+--	Place   VARCHAR(255)  NOT NULL,
+--	DataTime Date NOT NULL
+--);
+--INSERT INTO Begivenhed   VALUES (1,'J-pop Con','En con hvor vi fejre japensk pop kultur', 'DGI byen København', '2017-04-21');
+--INSERT INTO Begivenhed   VALUES (2,'Kengi','En con hvor vi fejre japensk pop kultur', 'Farum Arena Farum', '2017-06-30');
+INSERT INTO Begivenhed   VALUES (3,'Sakura Fest','Se sakura træerne springe ud', 'Langelinje København', '2017-04-28');
+
+
